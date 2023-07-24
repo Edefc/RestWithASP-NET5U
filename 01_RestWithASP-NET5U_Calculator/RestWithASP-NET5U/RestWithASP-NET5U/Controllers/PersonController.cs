860f28d6a687cce0eb61fabb5e3ff6f78257e380
Ede <edenilsonfc@gmail.com>
2023-07-24 20:39:29 -0300

    Working with four verbs

---
+++ 01_RestWithASP-NET5U_Calculator/RestWithASP-NET5U/RestWithASP-NET5U/Controllers/PersonController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestWithASP_NET5U.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+
+
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secundNumber}")]
+        public IActionResult Get(string firstNumber, string secundNumber)
+        {
+           
+            return BadRequest("Invalid Input");
+        }
+
+        
+
+    }
+}
